2ddc33e6e93d163638935bae83b0bbea47458d11
Yaroslav <yarkul777@gmail.com>
2025-06-28 23:08:55 +0200

    Removed tailwind. Project starts successfully. PrimeNg added.

--- yarbank-app/src/app/app.config.ts
+++ yarbank-app/src/app/app.config.ts
@@ -1,10 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+     provideAnimationsAsync(),
+     providePrimeNG({
+            theme: {
+                preset: Aura
+            }}),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
